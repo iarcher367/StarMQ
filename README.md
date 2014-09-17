5ec76ee8e61c7a670875088c33b78fbab6b8d064
Stephen Yu <celestial.paradigms@gmail.com>
2014-09-16 21:51:05 -0700

    add fluent configuration

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ StarMQ
 StarMQ exposes two primary APIs for messaging via **SimpleBus** and **AdvancedBus**. The Simple API provides few configuration options as it assumes default behaviour for common use-cases. For fine-grained control, the Advanced API offers methods that correlate to RabbitMQ’s 0-9-1 implementation.
 
 ## Highlights
-- The internal messaging architecture supports the addition of pre- and post-processing steps. Example pre-processing steps include message encryption, compression, and authentication. These may be toggled via configuration and registered at startup. At present, the only supported post-processing action is unsubscribing the current consumer.
+- The internal messaging architecture supports the addition of pre- and post-processing steps. Example pre-processing steps include message encryption, compression, and authentication. These may be enabled via configuration at startup. At present, the only supported post-processing action is unsubscribing the current consumer.
 - Consumers may control the client response to the broker by returning the appropriate Response object from the message handler. The Simple API exposes a basic method that takes care of the response by sending an _ack_ if the message handler successfully completes and a _nack_ if an exception bubbles out of the handler.
 - StarMQ supports **dead-lettering** and **alternate exchanges** using default settings and auto-generated exchange names.
 - StarMQ comes wired for **logging** with log4net. _Warning_: setting the log level below WARN reduces throughput by over 50%.
-- StarMQ uses SimpleInjector for **dependency injection** and is configured for overriding registrations. This allows easy replacement of any component by registering a custom implementation after calling Registration.RegisterServices(). For example, log4net could be replaced with another logger that implements the generic ILog interface found in log4net.
+- StarMQ uses SimpleInjector for **dependency injection** and is configured for overriding registrations. This allows easy replacement of any component by using OverrideRegistration to register the custom implementation. For example, log4net could be replaced with another logger that implements the generic ILog interface found in log4net.
 
 ## Performance
-- StarMQ can sustain a publishing throughput of ~20,000 messages per second with no pre-processing steps.
+- StarMQ's asynchronous internal architecture allows it to sustain a publishing throughput of ~20,000 messages per second with no pre-processing steps.
 - StarMQ automatically attempts to recover lost connections to the broker.
 - Publishes during a connection failure are non-blocking and buffered in memory until the connection is restored. _Warning_: high-throughput scenarios may cause memory issues during extended outages.
 - Message processing is independent among queues; a fast handler will finish processing all messages even if its messages are interleaved with messages for a slow handler.
@@ -20,3 +20,21 @@ StarMQ exposes two primary APIs for messaging via **SimpleBus** and **AdvancedBu
 - StarMQ offers guaranteed publishing via RabbitMQ’s publisher confirms.
 - RabbitMQ declines a message by sending a basic.nack, which StarMQ passes along by throwing a PublishException. Declines are typically caused by internal broker errors.
 - StarMQ waits a configurable timeout interval for a broker response. If the interval elapses, the message is re-published.
+
+## Quick-Start
+The Factory class allows fluent configuration and access to the SimpleBus API.
+```
+var simpleBus = new Factory()
+    .OverrideRegistration<ISerializationStrategy, MySerializationStrategy>()
+    .EnableCompression()
+    .EnableEncryption("AdAstraAndBeyond")
+    .AddInterceptor(new MyCustomInterceptor())
+    .GetBus(connectionString);
+```
+The example shows how to register custom implementations, enable built-in pre-processing steps, and insert custom steps (a.k.a. interceptors).
+
+With a SimpleBus, publishing and subscribing is as simple as:
+```
+simpleBus.PublishAsync("hello world", "my.routing.key").Wait(); // Wait() forces a synchronous call
+_simpleBus.SubscribeAsync<string>("id", new List<string> { "my.*" }, x => MyMessageHandler(x));
+```
---
+++ StarMQ/Factory.cs
@@ -0,0 +1,130 @@
+﻿namespace StarMQ
+{
+    using Core;
+    using log4net;
+    using Message;
+    using Publish;
+    using RabbitMQ.Client;
+    using SimpleInjector;
+    using SimpleInjector.Advanced.Extensions;
+    using System;
+    using System.Linq;
+    using IConnection = Core.IConnection;
+
+    public class Factory : IDisposable
+    {
+        internal Container Container { get; private set; }
+
+        public Factory()
+        {
+            Container = new Container();
+
+            Container.RegisterSingle<IAdvancedBus, AdvancedBus>();
+            Container.RegisterSingle<IConnection, PersistentConnection>();
+            Container.RegisterSingle<IConnectionConfiguration, ConnectionConfiguration>();
+            Container.RegisterSingle<IOutboundDispatcher, OutboundDispatcher>();
+            Container.RegisterSingle<IPipeline, InterceptorPipeline>();
+            Container.RegisterSingle<ISimpleBus, SimpleBus>();
+
+            Container.Register(() =>
+            {
+                var config = Container.GetInstance<IConnectionConfiguration>();
+                var connection = Container.GetInstance<IConnection>();
+
+                return config.PublisherConfirms
+                    ? new ConfirmPublisher(config, connection, LogManager.GetLogger(typeof(ConfirmPublisher)))
+                    : (IPublisher)new BasicPublisher(connection, LogManager.GetLogger(typeof(BasicPublisher)));
+            });
+
+            Container.Register(() =>
+            {
+                var config = Container.GetInstance<IConnectionConfiguration>();
+
+                return new ConnectionFactory
+                {
+                    HostName = config.Host,
+                    Password = config.Password,
+                    Port = config.Port,
+                    UserName = config.Username,
+                    VirtualHost = config.VirtualHost,
+                    RequestedHeartbeat = config.Heartbeat,
+                    RequestedConnectionTimeout = config.Timeout
+                };
+            });
+
+            Container.Register<ICorrelationStrategy, CorrelationStrategy>();
+            Container.Register<INamingStrategy, NamingStrategy>();
+            Container.Register<ISerializationStrategy, SerializationStrategy>();
+            Container.Register<ISerializer, JsonSerializer>();
+            Container.Register<ITypeNameSerializer, TypeNameSerializer>();
+
+            Container.RegisterWithContext(context => LogManager.GetLogger(context.ImplementationType));
+
+            Container.Options.AllowOverridingRegistrations = true;
+        }
+
+        public Factory AddInterceptor(IMessagingInterceptor interceptor)
+        {
+            if (interceptor == null)
+                throw new ArgumentNullException("interceptor");
+
+            Container.GetInstance<IPipeline>().Add(interceptor);
+            return this;
+        }
+
+        public Factory EnableCompression()
+        {
+            Container.GetInstance<IPipeline>().Add(new CompressionInterceptor());
+            return this;
+        }
+
+        /// <param name="secretKey">truncates to 128-bit key</param>
+        public Factory EnableEncryption(string secretKey)
+        {
+            if (secretKey == null)
+                throw new ArgumentNullException();
+            if (secretKey.Length < 16)
+                throw new ArgumentException("Secret key must be 16 characters in length.");
+
+            var key = new JsonSerializer().ToBytes(secretKey.Substring(0, 16));
+
+            Container.GetInstance<IPipeline>().Add(new AesInterceptor(key.Skip(1).Take(16).ToArray()));
+
+            return this;
+        }
+
+        public Factory OverrideRegistration<T, TK>() where T : class where TK : class, T
+        {
+            Container.Register<T, TK>();
+            return this;
+        }
+
+        /// <summary>
+        /// Creates a SimpleBus instance with the default configuration.
+        ///
+        /// cancelonhafailover=false;heartbeat=10;host=localhost;password=guest;port=5672;
+        /// publisherconfirms=false;reconnect=5000;timeout=10000;username=guest;virtualhost=/
+        /// </summary>
+        public ISimpleBus GetBus()
+        {
+            return Container.GetInstance<ISimpleBus>();
+        }
+
+        /// <summary>
+        /// Creates a SimpleBus instance with defaults overridden by any provided values.
+        /// </summary>
+        public ISimpleBus GetBus(string connectionString)
+        {
+            var configuration = Container.GetInstance<IConnectionConfiguration>();
+
+            Global.ParseConfiguration(configuration, connectionString);
+
+            return Container.GetInstance<ISimpleBus>();
+        }
+
+        public void Dispose()
+        {
+            Container.GetInstance<IConnection>().Dispose();
+        }
+    }
+}
