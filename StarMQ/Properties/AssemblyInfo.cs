94ec3a12ff976151ecd5cd30ad7b6188d90f3496
Stephen Yu <celestial.paradigms@gmail.com>
2014-09-28 12:15:33 -0700

    add nuspec, nupkg

--- StarMQ/Properties/AssemblyInfo.cs
+++ StarMQ/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StarMQ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An opinionated, highly-configurable .NET library for the popular RabbitMQ messaging broker.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stephen Yu")]
 [assembly: AssemblyProduct("StarMQ")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
 
 [assembly: InternalsVisibleTo("StarMQ.Test")]
